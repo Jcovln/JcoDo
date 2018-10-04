4a5ec0fffc6344aace1a6915aac8a00691eb5fec
Justin <Jkcox988@gmail.com>
2018-10-04 17:50:22 -0500

    override initial weback template

---
+++ src/components/JcoDo.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+  <h1>JcoDo</h1>
+  <p>{{ msg }}</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'JcoDo',
+  data () {
+    return {
+      msg: 'Welcome to your JcoDo List!'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ul {
+  list-style-type: none;
+}
+</style>
